677b402cafd9e63f0dc7ebe0141cd484930f839a
Anthony A <asanderson2009@gmail.com>
2019-08-18 19:02:11 -0400

    fixed css linking thanks to TA Patrick

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static('./app/public'))
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 require("./app/routing/apiRoutes")(app);
